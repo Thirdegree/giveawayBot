e75359188758d028843080f89e41773d7ba4ba45
Joel Christiansen <greenbadge.jc@gmail.com>
2015-04-06 21:10:02 -0700

    Testing results. Need to test methods involving actual reddit posting, as well as methods involving database.

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import praw, sqlite3
 from time import sleep
 
-giveawayString = "*MESSAGE TO BE SENT TO GIVEAWAY WINNERS HERE. PLACE '%s' WHEREVER YOU WANT THE GIVEAWAY REDEMTION KEY TO APPEAR"
+giveawayString = "*MESSAGE TO BE SENT TO GIVEAWAY WINNERS HERE. PLACE '%s's WHEREVER YOU WANT THE GIVEAWAY REDEMTION KEY AND KEY VALUE TO APPEAR (WILL APPEAR IN THAT ORDER)"
 conn = sqlite3.connect("giveaway.db")
 c = conn.cursor()
+r = praw.Reddit("Giveaway bot by /u/Thirdegree")
 
+#tested, works
 def create_database():
     c.execute('''CREATE TABLE IF NOT EXISTS giveaway
                 (username text, code text)''')
-    conn.execute()
+    conn.commit()
 
+#works
 def _login():
     USERNAME = raw_input("Username?\n> ")
     r.login(USERNAME, "")
     return USERNAME
 
+#works
 while True:
     try:
         USERNAME = _login()
@@ -23,42 +27,53 @@ while True:
     except praw.errors.InvalidUserPass:
         print "Invalid Username/password, please try again."
 
-
 def inbox():
     unread = r.get_unread()
     for i in unread:
         author = i.author
         if author==CONTROL:
-            giveaway(i.body)
+            if i.body.split("\n")[0].strip() == "+new giveaway":
+                #strips away the "+new giveaway line for the parser"
+                giveaway("\n".join(i.body.split("\n")[1:])) #check
         else:
-            dispowered_giveaway(author)
+            dispowered_giveaway(author) #check
         i.mark_as_read()
 
 def giveaway(body):
-    #{'username':'code'}
+    #{'username':['code', 'value']}
     to = parse(body)
+    add_database(to)
     for k, v in to.items():
-        send_message(k, "Giveaway prize", giveawayString%v)
+                                                        #code   value($)
+        send_message(k, "Giveaway prize", giveawayString%(v[0], v[1]))
         sleep(2)
 
+def add_database(userDict):
+    c.execute("DELETE FROM giveaway")
+    for k, v in userDict.items():
+        c.execute("INSERT INTO giveaway VALUES (?, ?)", (k, v))
+        conn.execute()
+
+#tested, works. Expects each line to be formatted "username: code value"
 def parse(body):
     temp = {}
     for line in body.split('\n'):
-        #['username', 'code']
+        #['username', 'code value']
         parse1 = line.split(":")
-        temp[parse1[0].strip()] = parse1[1].strip()
+        #{'username': ['code', 'value']}
+        temp[parse1[0].strip()] = parse1[1].strip().split()
     return temp
 
 def dispowered_giveaway(author):
-    response = c.execute("SELECT * FROM giveaway WHERE username=?", (author,))
-
-
-
-
+    response = c.execute("SELECT * FROM giveaway WHERE username=?", (author,)).fetchone()
+    if response:
+        send_message(author, "Giveaway code", "Your redemtion code is %s", response[1])
+    else:
+        send_message(author, "Giveaway code", "404'd! No code found. If you feel this message is in error, please contact /u/project_twenty5oh1")
 
 def main():
     while True:
-        inbox()
+        inbox() #check
         comments()
 
 
