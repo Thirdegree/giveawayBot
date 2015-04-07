7c82051d4587d20d741361a91342629b4bdd84ad
Joel Christiansen <greenbadge.jc@gmail.com>
2015-04-07 12:05:26 -0700

    Lots of testing, everything works so far

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import praw, sqlite3
 from time import sleep
 from random import choice
 
-giveawayString = "*MESSAGE TO BE SENT TO GIVEAWAY WINNERS HERE. PLACE '%s's WHEREVER YOU WANT THE GIVEAWAY REDEMTION KEY AND KEY VALUE TO APPEAR (WILL APPEAR IN THAT ORDER)"
+giveawayString = "*MESSAGE TO BE SENT TO GIVEAWAY WINNERS HERE. PLACE '%s's WHEREVER YOU WANT THE GIVEAWAY REDEMTION KEY AND KEY VALUE TO APPEAR (WILL APPEAR IN THAT ORDER)%s"
 conn = sqlite3.connect("giveaway.db")
 c = conn.cursor()
 sillies = ["*Silly responses need to be added here*", "Seperated by commas", "And surrounded by quotes"]
@@ -12,7 +12,7 @@ sillyTrigger = "Mr. Master"
 #tested, works
 def create_database():
     c.execute('''CREATE TABLE IF NOT EXISTS giveaway
-                (username text, code text)''')
+                (username text, code text, value text)''')
     conn.commit()
 
 #works
@@ -31,31 +31,35 @@ while True:
         print "Invalid Username/password, please try again."
 
 ################################
+#tested, works
 def inbox():
     unread = r.get_unread()
     for i in unread:
-        author = i.author
+        author = i.author.name
         if author==CONTROL:
             if i.body.split("\n")[0].strip() == "+new giveaway":
                 #strips away the "+new giveaway line for the parser"
-                giveaway("\n".join(i.body.split("\n")[1:])) #check
+                giveaway("\n".join(i.body.split("\n")[1:]).strip()) #check
         else:
             dispowered_giveaway(author) #check
         i.mark_as_read()
 
+#works, tested
 def giveaway(body):
     #{'username':['code', 'value']}
     to = parse(body)
     add_database(to)
     for k, v in to.items():
                                                         #code   value($)
-        send_message(k, "Giveaway prize", giveawayString%(v[0], v[1]))
+        r.send_message(k, "Giveaway prize", giveawayString%(v[0], v[1]))
 
+#works, tested
 def add_database(userDict):
     c.execute("DELETE FROM giveaway")
+    conn.commit()
     for k, v in userDict.items():
-        c.execute("INSERT INTO giveaway VALUES (?, ?)", (k, v))
-        conn.execute()
+        c.execute("INSERT INTO giveaway VALUES (?, ?, ?)", (k, v[0], v[1]))
+        conn.commit()
 
 #tested, works. Expects each line to be formatted "username: code value"
 def parse(body):
@@ -67,15 +71,17 @@ def parse(body):
         temp[parse1[0].strip()] = parse1[1].strip().split()
     return temp
 
+#tested, works.
 def dispowered_giveaway(author):
     response = c.execute("SELECT * FROM giveaway WHERE username=?", (author,)).fetchone()
     if response:
-        send_message(author, "Giveaway code", "Your redemtion code is %s", response[1])
+        r.send_message(author, "Giveaway code", "Your redemtion code is %s"%response[1])
     else:
-        send_message(author, "Giveaway code", "404'd! No code found. If you feel this message is in error, please contact /u/project_twenty5oh1")
+        r.send_message(author, "Giveaway code", "404'd! No code found. If you feel this message is in error, please contact /u/project_twenty5oh1")
 ################################
 
 ################################
+#works, tested
 def comments():
     comment_stream = praw.helpers.comment_stream(r, 'all')
     i=0
@@ -86,6 +92,7 @@ def comments():
             break
         i=i+1
 
+#works, tested
 def silly(comment):
     comment.reply(choice(sillies))
 ################################
@@ -94,7 +101,7 @@ def silly(comment):
 def main():
     while True:
         inbox() #check
-        comments()
+        comments() #check
 
 
 if __name__ == '__main__':
